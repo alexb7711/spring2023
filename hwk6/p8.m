a40c56f7fc931c1af783506ed79346ea0f3a24e5
Alexander Brown <alex.brown7711@gmail.com>
2023-03-20 12:43:51 -0600

    Done with 8

--- hwk6/p8.m
+++ hwk6/p8.m
@@ -58,3 +58,50 @@ hold off;
 
 ##==============================================================================
 ## (c) Plot frequency response
+t = 1:100;
+figure;
+hold on;
+
+Hc = [];
+He = [];
+
+for i = t
+  Hc = [Hc, norm(1/(1-0.4*exp(-i*1j)))];
+  He = [He, norm(h(1) + h(2)*exp(-i.*1j))];
+end
+
+plot(t,Hc);
+plot(t,He);
+
+hold off;
+
+##==============================================================================
+## (c) Plot frequency response
+x0 = 1;
+N  = 1000;
+t  = [1:N];
+
+## Propagate d
+d = [];
+d = [d, x0];
+
+for i = 2:N
+  d = [d, 0.3*d(i-1) + random("normal",0,sn,[1,1])];
+end
+
+## Propagate dhat
+dh = [];
+dh = [dh, x0];
+
+for i = 2:N
+  dh = [dh, 0.25*h(1) + h(2)*0.5*dh(i-1) + random("normal", 0, sn, [1,1])];
+end
+
+## Plot responses
+figure;
+hold on;
+plot(t,d)
+plot(t,dh)
+hold off;
+
+## close all;
