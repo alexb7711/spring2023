11056784a4fed7317327ff23d88a02ca5471ad57
Alexander Brown <alex.brown7711@gmail.com>
2023-03-20 12:23:52 -0600

    working

--- hwk6/p8.m
+++ hwk6/p8.m
@@ -4,35 +4,33 @@
 rng(1);                                                                         # Seed random number generator
 
 ## Parametors
-## a1 = -0.7;
-## a2 = 0.12;
 a1 = -0.7;
 a2 = 0.12;
-sv = 0.2;
-sn = 0.1;
+sv = 0.1;
+sn = 0.16;
 
 ## Calculate r values
-r0 = (1+a2)/(1-a2) * (sv)/((1+a2)^2 - a1^2)
-r1 = -a1/(1+a2)*sv
+r0 = (1+a2)/(1-a2) * (sv)/((1+a2)^2 - a1^2);
+r1 = -a1/(1+a2)*r0;
 
 ## Create Rzz matrix
 Rnn = sn*eye(2);
 Ruu = [r0 r1;
-       r0 r1];
+       r1 r0];
 Rzz = Rnn + Ruu;
 
 ## Calculate p vector
-p    = [0,0];
-p(1) = r0 - 0.4*r1
-p(2) = r1 - 0.4*r0
+p    = [0;0];
+p(1) = r0 - 0.4*r1;
+p(2) = r1 - 0.4*r0;
 
 ## Solve for coefficients
-h = pinv(Rzz)*p'
+h = inv(Rzz)*p
 
 ##==============================================================================
 ## (b) Simulate
 x0 = 1;
-N  = 4;
+N  = 1000;
 t  = [1:N];
 
 ## Propagate d
@@ -40,7 +38,7 @@ d = [];
 d = [d, x0];
 
 for i = 2:N
-  d = [d, 0.3*d(i-1)];
+  d = [d, 0.3*d(i-1) + random("normal",0,sn,[1,1])];
 end
 
 ## Propagate dhat
@@ -48,9 +46,10 @@ dh = [];
 dh = [dh, x0];
 
 for i = 2:N
-  dh = [dh, h(1) + h(2)*dh(i-1) + random("normal", 0, sn, [N,4])];
+  dh = [dh, h(1) + h(2)*dh(i-1) + random("normal", 0, sn, [1,1])];
 end
 
+## Plot responses
 clf reset;
 hold on;
 plot(t,d)
