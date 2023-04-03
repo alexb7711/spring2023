c685a51c11617370b63c6f8989f0d34ac7100a29
Alexander Brown <alex.brown7711@gmail.com>
2023-04-03 09:17:27 -0600

    add in step function to impulse

--- midterm/p9.m
+++ midterm/p9.m
@@ -5,7 +5,7 @@ syms t c;
 ip = @(x,y) vpa(int(x*y,t,[0,3]));
 
 %% Impulse response function
-h = @(x) (exp(-4*x) + exp(-3*x));
+h = @(x) (exp(-4*x) + exp(-3*x)).*heaviside(x);
 
 %% y1
 y1 = h(3-t)
@@ -16,8 +16,8 @@ C  = solve(subs(y2,t,0) + c == 0);
 y2 = subs(y2 + C,t,3-t)
 
 %% Grammian
-R = double([ip(y1,y1), ip(y1,y2);
-            ip(y1,y2), ip(y2,y2)])
+R = double([ip(y1,y1), ip(y1,y2),
+            ip(y1,y2), ip(y2,y2)]);
 p = [2;4];
 
 %% Solve
@@ -28,20 +28,19 @@ x = @(x) c(1)*(exp(-4*(3-x)) + exp(-3*(3-x))) + ...
          c(2)*(7/12 - 1/4*exp(-4*(3-x)) - 1/3*exp(-3*(3-x)));
 
 %% Plot parameters
-N = 1000;
-t = linspace(0,4);
+t = linspace(0,3);
 xd = x(t);
 hd = h(t);
 
 %% Plot input signal
 subplot(3,1,1);
-plot(xd, "LineWidth", 2);
+plot(t,xd);
 xlabel('t');
 ylabel('x(t)');
 
 %% Plot impulse response
 subplot(3,1,2);
-plot(hd, "LineWidth", 2);
+plot(t,hd);
 xlabel('t');
 ylabel('h(t)');
 
@@ -50,7 +49,7 @@ y = conv(xd,hd,'same');
 
 %% Plot output signal
 subplot(3,1,3);
-plot(y,"r-",'LineWidth',2)
+plot(t,y,"r-")
 xlabel('t');
 ylabel('y(t)');
 
